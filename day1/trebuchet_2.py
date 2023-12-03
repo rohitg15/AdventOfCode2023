d483de0a8f98c24d0cd177574d3833607a5ba7ad
Rohit Gurunath <rohitgurunath@ghost-MacBook-Pro.local>
2023-12-02 16:15:36 -0800

    updated comments for trebuchet_2

--- day1/trebuchet_2.py
+++ day1/trebuchet_2.py
@@ -2,39 +2,47 @@ from curses.ascii import isdigit
 import sys
 
 # https://adventofcode.com/2023/day/1#part2
+
 def get_first_digit(line: str, l2r: bool) -> chr:
 
     digit_map = {
-        'seven': '7',
-        'nine': '9',
-        'eight': '8',
-        'two': '2',
         'one': '1',
+        'two': '2',
         'three': '3',
         'four': '4',
         'five': '5',
         'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
     }
 
     l = len(line)
     i = 0 if l2r else l - 1
     res = []
     while True:
+        # iterate from l2r or r2l as indicated
         if l2r and i >= l:
             break
         if not l2r and i < 0:
             break
 
+        # examine strings of length 3, 4, 5 against digit_map
+        # since those are the sizes of the values in the digit_map. 
         match_set = (line[i: i+3], line[i: i+4], line[i:i+5]) if l2r else (line[i: i-3: -1], line[i: i-4: -1], line[i:i-5:-1])
         is_replaced = False
         for j, pkey in enumerate(match_set):
             key = pkey if l2r else pkey[::-1]
             if digit_map.get(key) is not None:
+                # if a match is found, replace by digit in the result set
+                # and update iterator by corresponding length
                 res.append(digit_map[key])
                 i = i + j + 3 if l2r else i - j - 3
                 is_replaced = True
                 break
         
+        # if no match from match_set, then add current character 
+        # to the result set and move to next character
         if not is_replaced:
             res.append(line[i])
             i = i + 1 if l2r else i - 1
@@ -48,12 +56,15 @@ if __name__ == "__main__":
         print (f'Usage: {sys.argv[0]} input_file')
         sys.exit(-1)
     
+    # parse input lines from file
     file_name = sys.argv[1]
     lines = []
     with open(file_name, 'r') as file:
         lines = file.readlines()
 
-    get_digits = lambda line : int(get_first_digit(line, True) + get_first_digit(line, False))
+    # examine from l2r and r2l to handle the following
+    # eg: oneight must resolve to 18
+    get_digits = lambda line : int(get_first_digit(line, l2r=True) + get_first_digit(line, l2r=False))
     
     s = sum(get_digits(line.strip('\n'))  for line in lines)
     print (s)
